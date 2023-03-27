99d5f3baaadf0bf508cdafef6b93e2e41d14b848
Ronan Cimadure <ronan.cimadure@gmail.com>
2023-03-27 18:48:47 -0400

    reformant

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
 import numpy as np
-#from docplex.mp.advmodel import AdvModel
+# from docplex.mp.advmodel import AdvModel
 from docplex.mp.model import Model
 
-# content of test_sample.py
+
 def func(x):
     return x + 1
-
 
 
 """
@@ -16,102 +15,94 @@ Par année:
 -tableau: valeurs des coûts 
 """
 
-
 """
 		[self.add_range(lb=self.params['SOCmin'], expr='SOC__{n}{i}'.format(n=n,i=i), ub=self.params['SOCmax']) for n in self.ens['N'] for i in self.ens['I']]
 """
 
 
-
 # import cplex._internal._procedural as proc
-#print(proc.__file__)
+# print(proc.__file__)
 
 
 # https://ibmdecisionoptimization.github.io/docplex-doc/mp/docplex.mp.model.html
 # https://notebook.community/IBMDecisionOptimization/docplex-examples/examples/mp/jupyter/tutorials/Beyond_Linear_Programming
 class ModelShaving(Model):
-	
-		
-	def __init__(self, *args, **kwargs):
-		self.params = kwargs.pop('params',None)
-		if self.params:
-			self.ens = {"utilsateur":np.shape(self.params['Rut'])[0], # i
-			"borne":np.shape(self.params['Pch_max_n'])[0], # n
-			 "instant":np.shape(self.params['Pb'])[0], # t
-			 "mois":12} # m			 
-			self.ens['I'] = range(1,self.ens['utilsateur']+1)
-			self.ens['N'] = range(1,self.ens['borne']+1)
-			self.ens['T'] = range(0,self.ens['instant'])
-			self.ens['M'] = range(1,self.ens['mois']+1)
-			#print(self.ens)
-						
-		super(ModelShaving, self).__init__(*args, **kwargs)
-		
 
-	def problem_variables(self):
-			
-		"""
-		SOCi(t)	État de charge de la flotte des véhicules électriques à l’instant t	kWh
-		Pch,ni(t)	Puissance de charge à l’instant (t)	kW
-		Pdis,ni(t)	Puissance de décharge à l’instant (t)	kW
-		delta_ch,i(t)	1, Si le profil d’utilisateur i est en charge
-		0, Sinon	Bin
-		delta_dis,i(t)	1, Si le profil d’utilisateur i est en décharge
-		0, Sinon	Bin
-		Rborne,ni	Ratio de borne de type n utilisé par le profil d’utilisateur i	%
-		"""
-		self.SOC__n_i_t = self.continuous_var_cube(keys1=self.ens['N'], keys2=self.ens['I'], keys3=self.ens['T'], lb=0, ub=self.params['MAX_OPTIM'], name='SOC_')#, key_format=None)
+    def __init__(self, *args, **kwargs):
+        self.params = kwargs.pop('params', None)
+        if self.params:
+            self.ens = {"utilsateur": np.shape(self.params['Rut'])[0],  # i
+                        "borne": np.shape(self.params['Pch_max_n'])[0],  # n
+                        "instant": np.shape(self.params['Pb'])[0],  # t
+                        "mois": 12}  # m
+            self.ens['I'] = range(1, self.ens['utilsateur'] + 1)
+            self.ens['N'] = range(1, self.ens['borne'] + 1)
+            self.ens['T'] = range(0, self.ens['instant'])
+            self.ens['M'] = range(1, self.ens['mois'] + 1)
+        # print(self.ens)
 
-		self.Pch__n_i_t = self.continuous_var_cube(keys1=self.ens['N'], keys2=self.ens['I'], keys3=self.ens['T'], lb=0, ub=self.params['MAX_OPTIM'], name='Pch_')#, key_format=None)
-		self.Pdis__n_i_t = self.continuous_var_cube(keys1=self.ens['N'], keys2=self.ens['I'], keys3=self.ens['T'], lb=0, ub=self.params['MAX_OPTIM'], name='Pdis_')#, key_format=None)
-		
-		self.delta_ch__i_t = self.binary_var_matrix(self.ens['I'], self.ens['T'], name='delta_ch_')
-		self.delta_dis__i_t = self.binary_var_matrix(self.ens['I'], self.ens['T'], name='delta_dis_')
+        super(ModelShaving, self).__init__(*args, **kwargs)
 
-		self.Rborne__n_i = self.binary_var_matrix(self.ens['N'], self.ens['I'], name='Rborne_')
+    def problem_variables(self):
+        """
+        SOCi(t)	État de charge de la flotte des véhicules électriques à l’instant t	kWh
+        Pch,ni(t)	Puissance de charge à l’instant (t)	kW
+        Pdis,ni(t)	Puissance de décharge à l’instant (t)	kW
+        delta_ch,i(t)	1, Si le profil d’utilisateur i est en charge
+        0, Sinon	Bin
+        delta_dis,i(t)	1, Si le profil d’utilisateur i est en décharge
+        0, Sinon	Bin
+        Rborne,ni	Ratio de borne de type n utilisé par le profil d’utilisateur i	%
+        """
+        self.SOC__n_i_t = self.continuous_var_cube(keys1=self.ens['N'], keys2=self.ens['I'], keys3=self.ens['T'], lb=0,
+                                                   ub=self.params['MAX_OPTIM'], name='SOC_')  # , key_format=None)
 
+        self.Pch__n_i_t = self.continuous_var_cube(keys1=self.ens['N'], keys2=self.ens['I'], keys3=self.ens['T'], lb=0,
+                                                   ub=self.params['MAX_OPTIM'], name='Pch_')  # , key_format=None)
+        self.Pdis__n_i_t = self.continuous_var_cube(keys1=self.ens['N'], keys2=self.ens['I'], keys3=self.ens['T'], lb=0,
+                                                    ub=self.params['MAX_OPTIM'], name='Pdis_')  # , key_format=None)
 
-	def problem_constraint_prevent_simultaneous_charge_and_discharge(self):
-		[self.add_constraint(self.delta_ch__i_t[i,t] + self.delta_dis__i_t[i,t] <= 1.0) for i in self.ens['I'] for t in self.ens['T']]
-				
-	def problem_constraint_SOC_range(self):
-		pass
+        self.delta_ch__i_t = self.binary_var_matrix(self.ens['I'], self.ens['T'], name='delta_ch_')
+        self.delta_dis__i_t = self.binary_var_matrix(self.ens['I'], self.ens['T'], name='delta_dis_')
 
-	def problem_constraint_Pch_range(self):
-		pass
-				
-	def problem_constraint_Pdis_range(self):
-		pass
+        self.Rborne__n_i = self.binary_var_matrix(self.ens['N'], self.ens['I'], name='Rborne_')
 
-	def problem_power_aggration__t(s_i,delta_i,r__ut_i,p__n_i, r__n_i):
-		pass
+    def problem_constraint_prevent_simultaneous_charge_and_discharge(self):
+        [self.add_constraint(self.delta_ch__i_t[i, t] + self.delta_dis__i_t[i, t] <= 1.0) for i in self.ens['I'] for t
+         in self.ens['T']]
 
-	def problem_constraint_Pch_total__t(self):
-		pass
-		
-	def problem_constraint_Pdis_total__t(self):
-		pass		
+    def problem_constraint_SOC_range(self):
+        pass
 
-	def problem_constraint_Pch__n_i_t(self):
-		pass
-		
-	def problem_constraint_Pdis__n_i_t(self):
-		pass		
+    def problem_constraint_Pch_range(self):
+        pass
 
+    def problem_constraint_Pdis_range(self):
+        pass
 
-	def problem_constraint_SOC__n_i_t(self):
-		pass
+    def problem_power_aggration__t(s_i, delta_i, r__ut_i, p__n_i, r__n_i):
+        pass
 
+    def problem_constraint_Pch_total__t(self):
+        pass
 
-	def problem_constrainsts(self):
-		self.problem_constraint_prevent_simultaneous_charge_and_discharge()
-		
-		#[self.add_range(lb=self.params['SOCmin'], expr='SOC__{n}{i}'.format(n=n,i=i), ub=self.params['SOCmax']) for n in self.ens['N'] for i in self.ens['I']]
-		#[self.add_range(lb=self.params['SOCmin'], expr=self.SOC__n_i_t[n,i], ub=self.params['SOCmax']) for n in self.ens['N'] for i in self.ens['I']];
-		#[self.le_constraint(self.delta_ch__i_t[i,t] + self.delta_dis__i_t[i,t], 1.0, name='ctr_ch_dis') for i in self.ens['I'] for t in self.ens['T']]
-	
+    def problem_constraint_Pdis_total__t(self):
+        pass
 
+    def problem_constraint_Pch__n_i_t(self):
+        pass
 
+    def problem_constraint_Pdis__n_i_t(self):
+        pass
 
+    def problem_constraint_SOC__n_i_t(self):
+        pass
 
-#%%
+    def problem_constrainsts(self):
+        self.problem_constraint_prevent_simultaneous_charge_and_discharge()
+
+# [self.add_range(lb=self.params['SOCmin'], expr='SOC__{n}{i}'.format(n=n,i=i), ub=self.params['SOCmax']) for n in self.ens['N'] for i in self.ens['I']]
+# [self.add_range(lb=self.params['SOCmin'], expr=self.SOC__n_i_t[n,i], ub=self.params['SOCmax']) for n in self.ens['N'] for i in self.ens['I']];
+# [self.le_constraint(self.delta_ch__i_t[i,t] + self.delta_dis__i_t[i,t], 1.0, name='ctr_ch_dis') for i in self.ens['I'] for t in self.ens['T']]
+
+# %%
--- problem.py
+++ problem.py
@@ -1,9 +1,10 @@
 from model import ModelShaving
-#from docplex.mp.advmodel import AdvModel
+# from docplex.mp.advmodel import AdvModel
 import numpy as np
 import pandas as pd
 import sys
-import cplex
+
+# import cplex
 
 print('Load')
 
@@ -20,47 +21,45 @@ Pb = an['Power Clipped [kW]'].to_numpy(copy=True)
 Pb = Pb[:11]
 
 print(Pb.shape)
-#print(df.info)
-#[69504 rows x 14 columns]>
+# print(df.info)
+# [69504 rows x 14 columns]>
 
 print('Set problem parameters')
 
-
 params = {
-#Variables	Descriptions	Unités
-'SOCmin':20.0, #	État de charge minimum des véhicules électriques	kWh
-'SOCmax':80.0, #	État de charge maximum des véhicules électriques	kWh
-'Pch_min': 5.0	,#Puissance de charge minimum 	kW
-'Pch_max_n' : np.array([[7.5],[50.0]]) ,#Puissance de charge maximum en utilisant les bornes de type n	kW
-'Pdis_min': 5.0	,#Puissance de décharge minimum	kW
-'Pdis_max_n' : np.array([[5.0],[40.0]])	,#Puissance de décharge maximum en utilisant les bornes de type n	kW
-'beta_ch':0.90	,#Efficacité de charge	%
-'beta_dis':0.90	,#Efficacité de décharge	%
-'NEVs':1000	,#Nombre de véhicules électriques	Qté
-'Rut' : np.array([[0.25],[0.25],[0.25], [0.25]])	,#Ratio d’utilisateurs avec le profil i	%
+    # Variables	Descriptions	Unités
+    'SOCmin': 20.0,  # État de charge minimum des véhicules électriques	kWh
+    'SOCmax': 80.0,  # État de charge maximum des véhicules électriques	kWh
+    'Pch_min': 5.0,  # Puissance de charge minimum 	kW
+    'Pch_max_n': np.array([[7.5], [50.0]]),  # Puissance de charge maximum en utilisant les bornes de type n	kW
+    'Pdis_min': 5.0,  # Puissance de décharge minimum	kW
+    'Pdis_max_n': np.array([[5.0], [40.0]]),  # Puissance de décharge maximum en utilisant les bornes de type n	kW
+    'beta_ch': 0.90,  # Efficacité de charge	%
+    'beta_dis': 0.90,  # Efficacité de décharge	%
+    'NEVs': 1000,  # Nombre de véhicules électriques	Qté
+    'Rut': np.array([[0.25], [0.25], [0.25], [0.25]]),  # Ratio d’utilisateurs avec le profil i	%
 
-'delta_t' : 15/60 , # 15mins
-'MAX_OPTIM' : 1e8, # Facteur majorant du programme d'optimisation
+    'delta_t': 15 / 60,  # 15mins
+    'MAX_OPTIM': 1e8,  # Facteur majorant du programme d'optimisation
 
-#Puissance appelée par le bâtiment appelée à l’instant t	kW
-'Pb' : Pb	
+    'Pb': Pb,  # Puissance appelée par le bâtiment appelée à l’instant t	kW
+
+    # Si(t)  #1, Si le profil d’utilisateur est raccordé à une borne à l’instant t
 
 }
 
-
 mdl = ModelShaving('V2B', params=params)
 mdl.problem_variables();
-mdl.problem_constrainsts();	
+mdl.problem_constrainsts();
 
-#mdl.add_kpi(mdl.max(mdl.v), "Max Pch(t)");
+# mdl.add_kpi(mdl.max(mdl.v), "Max Pch(t)");
 
 print(mdl.print_information())
-
 
 solus = mdl.solve()
 print("Obj", mdl.solution.get_objective_value())
 print("--------------------------------------------")
 print(mdl.print_solution(print_zeros=False))
 print(solus)
+print(mdl.describe_objectives())
 print('end')
-
