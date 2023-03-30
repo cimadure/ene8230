064b5a076025014e07b47db9e27f257a7c008920
ronan <ronan.cimadure@gmail.com>
2023-03-29 20:31:25 -0400

    problem information

--- problem.py
+++ problem.py
@@ -11,44 +11,50 @@ print('Load')
 print('Load Pb and other data')
 filename = "ConsommationUniversite.pickle"
 df = pd.read_pickle(filename)
-
+print(df.shape)
 year = df['year'].unique()
 print(year)
 
 an = df[df['year'] == year[0]]
 Pb = an['Power Clipped [kW]'].to_numpy(copy=True)
 
-Pb = Pb[:70]
+Pb = Pb[:40]
 
 print(Pb.shape)
+
 # print(df.info)
 # [69504 rows x 14 columns]>
 
 l = Pb.shape[0]
+print("maximum de Pb(t):{m} avec T={t}".format(m=max(Pb), t=l))
 #Si = 10 * np.random.random_sample((l, 4)) - 5
-Si = np.random.randint(0, 2, size=(l, 1))
+#Si = np.random.randint(0, 2, size=(l, 1))
+Si = np.ones((l, 4))
 #print(Si)
 
 print('Set problem parameters')
-
+n_ev = 1000.0
 # Prius 4.4kWh, Leaf 24kWh
+soc_max = 0.8*24*1000
+soc_min = 0.2*4.4*1000
+print("{mi} <= SOC <= {ma}".format(mi=soc_min, ma=soc_max))
 
 params = {
     # Variables	Descriptions	Unités
-    'SOCmin': 20.0,  # État de charge minimum des véhicules électriques	kWh
-    'SOCmax': 80.0,  # État de charge maximum des véhicules électriques	kWh
+    'SOCmin': soc_min,  # État de charge minimum des véhicules électriques	kWh
+    'SOCmax': soc_max,  # État de charge maximum des véhicules électriques	kWh
     'Pch_min': 5.0,  # Puissance de charge minimum 	kW
     'Pdis_min': 5.0,  # Puissance de décharge minimum	kW
-    #'Pch_max_n': [7.5, 50.0],  # Puissance de charge maximum en utilisant les bornes de type n	kW
-    #'Pdis_max_n': [5.0, 40.0],  # Puissance de décharge maximum en utilisant les bornes de type n	kW
-    'Pch_max_n': [50.0],  # Puissance de charge maximum en utilisant les bornes de type n	kW
-    'Pdis_max_n': [40.0],  # Puissance de décharge maximum en utilisant les bornes de type n	kW
+    'Pch_max_n': [7.5, 50.0],  # Puissance de charge maximum en utilisant les bornes de type n	kW
+    'Pdis_max_n': [5.0, 40.0],  # Puissance de décharge maximum en utilisant les bornes de type n	kW
+    #'Pch_max_n': [50.0],  # Puissance de charge maximum en utilisant les bornes de type n	kW
+    #'Pdis_max_n': [40.0],  # Puissance de décharge maximum en utilisant les bornes de type n	kW
 
     'beta_ch': 0.93,  # Efficacité de charge	%
     'beta_dis': 0.90,  # Efficacité de décharge	%
-    'NEVs': 1000.0,  # Nombre de véhicules électriques	Qté
-    #'Rut': [0.21, 0.22, 0.23, 0.34],  # Ratio d’utilisateurs avec le profil i	%
-    'Rut': [1.0],  # Ratio d’utilisateurs avec le profil i	%
+    'NEVs': n_ev,  # Nombre de véhicules électriques	Qté
+    'Rut': [0.21, 0.22, 0.23, 0.34],  # Ratio d’utilisateurs avec le profil i	%
+    #'Rut': [1.0],  # Ratio d’utilisateurs avec le profil i	%
 
     'delta_t': 15 / 60,  # 15mins
     'MAX_OPTIM': 1e8,  # Facteur majorant du programme d'optimisation
